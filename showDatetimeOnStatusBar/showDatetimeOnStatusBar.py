eb1d69afef055117787b9322f32ee0694d9c284d
meizhichao <meizhichao@baiwang.com>
2017-04-26 19:39:21 +0800

    失去焦点时结束线程

--- showDatetimeOnStatusBar/showDatetimeOnStatusBar.py
+++ showDatetimeOnStatusBar/showDatetimeOnStatusBar.py
@@ -11,12 +11,15 @@ class ShowDatetime(sublime_plugin.EventListener):
 
     def __set_timer(self, view):
         self.timer[view.id()] = True
-        while True:
+        while self.timer[view.id()]:
             current_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y/%m/%d %H:%M:%S')
             view.set_status('acurrent_time', current_time)
             time.sleep(1)
 
     def on_activated_async(self, view):
         if not self.timer.get(view.id(), False):
-            t = threading.Thread(target=self.__set_timer, args=(view,))
+            t = threading.Thread(target=self.__set_timer, name='timer' + str(view.id()), args=(view,))
             t.start()
+
+    def on_deactivated_async(self, view):
+        self.timer[view.id()] = False
